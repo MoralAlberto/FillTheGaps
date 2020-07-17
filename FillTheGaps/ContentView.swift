68120dbbf02c965b8d0111f80e1e4284948d817e
Alberto Moral <alberto.moral.g@gmail.com>
2020-07-17 19:02:43 +0200

    Initial Commit

---
+++ FillTheGaps/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FillTheGaps
+//
+//  Created by Home on 17/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FillTheGaps/FillTheGapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FillTheGapsApp.swift
+//  FillTheGaps
+//
+//  Created by Home on 17/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct FillTheGapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
